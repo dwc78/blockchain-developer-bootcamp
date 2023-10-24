5648fbb67735facedeeacd65c62e5edd85715d03
dave clemmons <dclemmons@gmail.com>
2023-10-24 12:21:45 -0400

    deploy toeken contract

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -3,7 +3,7 @@ async function main() {
    // fetch contract to deploy
     const Token = await ethers.getContractFactory("Token")
     
-   // deploy contract
+   // deploy token contract
    const token = await Token.deploy()
    await token.deployed()
    console.log(`Token Deployed to: ${token.address}`)
