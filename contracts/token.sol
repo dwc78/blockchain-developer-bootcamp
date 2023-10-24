0e3999dd4fa11c9acb479a22617de7fa4a99ac6b
dave clemmons <dclemmons@gmail.com>
2023-10-23 21:07:00 -0400

    smoketest

---
+++ contracts/token.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+
+//always declare the version 1st
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// then name your contract
+contract Token {
+    string public name = "My Token";
+
+}
+// save with a space at the end of file
---
+++ scripts/1_deploy.js
@@ -0,0 +1,18 @@
+
+async function main() {
+   // fetch contract to deploy
+    const Token = await ethers.getContractFactory("Token")
+    
+   // deploy contract
+   const token = await Token.deploy()
+   await token.deployed()
+   console.log(`Token Deployed to: ${token.address}`)
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
