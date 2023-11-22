157eb5acb850cfe278707399e07f2b64f92f388a
dave clemmons <dclemmons@gmail.com>
2023-11-22 17:29:21 -0500

    rmoved greet_and _lock

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,13 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 
+// created erc-20 token about - 9:05 in the smoke test video
+
 //always declare the version 1st
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// then name your contract
+// then name your contract - State veriable, belongs to the contracts state, written to BC
 contract Token {
-    string public name = "My Token";
+    string public name = "Tesla Dave";
     
 }
 // save with a space at the end of file
