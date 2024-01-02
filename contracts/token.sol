608aa9d0da5b283e84b6b8bdaeeff6c2c3279517
dwc78 <dclemmons@gmail.com>
2024-01-02 15:41:36 -0500

    contract- 1-2-24

--- contracts/token.sol
+++ contracts/token.sol
@@ -8,17 +8,26 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 // then name your contract - State variable, belongs to the contracts state, written to BC
+// same as uint256 public totalSupply = 1000000 * (10**decimals); //same as 1,000,000 X 10^18;
 contract Token {
     string public name;
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
-    // same as uint256 public totalSupply = 1000000 * (10**decimals); //same as 1,000,000 X 10^18;
+    
+
+    //track balances
+    mapping (address => uint256) public balanceOf;
+
+    // send tokens
+
 
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        // this allows us to write information to the mapping, reading would be uint256 = balanceOf[msg.sender] = totalSupply;
+        balanceOf[msg.sender] = totalSupply;
     }
 }
 // save with a space at the end of file
--- test/Token.js
+++ test/Token.js
@@ -9,39 +9,51 @@ const tokens = (n) => {
 describe('Token', () => {
     // tests go inside here, "token" is calling the token.sol
     let token
+    let accounts
+    let deployer
 
     beforeEach(async () => {
         //code get executed here that get executed before each...
          
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Tesla Dave', 'ROCKETCAR', '1000000')
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
 })
 
-    //describe('Deployment', () => {
-    // const name = 'Tesla Dave'
-    // const symbol = 'ROCKETCAR'
-   // const decimals = '18'
-   // const totalSupply = tokens('1000000')
+describe('Deployment', () => {
+    const name = 'Tesla Dave'
+    const symbol = 'ROCKETCAR'
+    const decimals = '18'
+    const totalSupply = tokens('1000000')
 
     //create containing function - add async
     it('has correct name', async () => {
     //Check that token name is correct - use Chai matchers here...
-    expect(await token.name()).to.equal('Tesla Dave')
+    expect(await token.name()).to.equal(name)
     })
     
     it ('has correct symbol', async () => {
     //Check that token symbol is correct - use Chai matchers here...
-    expect(await token.symbol()).to.equal('ROCKETCAR')
+    expect(await token.symbol()).to.equal(symbol)
     })
     
     it('has correct decimals', async () => {
     //Check that token decimal count is correct - use Chai matchers here...
-    expect (await token.decimals()).to.equal('18')
+    expect (await token.decimals()).to.equal(decimals)
     })
     
     it ('has correct total Supply', async () => {
     //Check that token total supply count is correct - use Chai matchers here...
-    expect(await token.totalSupply()).to.equal(tokens('1000000'))
+    expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+    it ('assigns total Supply to Deployer', async () => {
+    //Check that token total supply count is correct - use Chai matchers here...
+    // console.log(deployer.address)
+    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 
     })
+})
 })
